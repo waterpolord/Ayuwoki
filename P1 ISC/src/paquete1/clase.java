8cd859aa4c2da7a5a47857a3adc281c0eb662696
Arlenys Cuevas <Arlenys Cuevas@Arlenycj>
2019-03-05 15:52:46 -0400

    hola

--- P1 ISC/src/paquete1/clase.java
+++ P1 ISC/src/paquete1/clase.java
@@ -2,4 +2,14 @@ package paquete1;
 
 public class clase {
 
+	private int arleny;
+
+	public int getArleny() {
+		return arleny;
+	}
+
+	public void setArleny(int arleny) {
+		this.arleny = arleny;
+	}
+	
 }
