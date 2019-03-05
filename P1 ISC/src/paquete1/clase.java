316ea2e217f038a53a4d11b9da4b01ff0f102f6c
Patricia <Patricia@Camarena-PC>
2019-03-05 15:56:42 -0400

    update

--- P1 ISC/src/paquete1/clase.java
+++ P1 ISC/src/paquete1/clase.java
@@ -3,6 +3,16 @@ package paquete1;
 public class clase {
 
 	private int arleny;
+	private int hello;
+	
+
+	public int getHello() {
+		return hello;
+	}
+
+	public void setHello(int hello) {
+		this.hello = hello;
+	}
 
 	public int getArleny() {
 		return arleny;
