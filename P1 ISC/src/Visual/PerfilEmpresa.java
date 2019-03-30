08c21d1cfbe04d228865afc7c6aaf887c875b84d
Arlenys Cuevas <Arlenys Cuevas@Arlenycj>
2019-03-30 01:26:52 -0400

    Creacion de los Perfiles

--- P1 ISC/src/Visual/PerfilEmpresa.java
+++ P1 ISC/src/Visual/PerfilEmpresa.java
@@ -6,10 +6,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import java.awt.SystemColor;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.border.MatteBorder;
+import javax.swing.JRadioButton;
+import javax.swing.DefaultComboBoxModel;
 
 public class PerfilEmpresa extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -31,12 +44,88 @@ public class PerfilEmpresa extends JFrame {
 	 * Create the frame.
 	 */
 	public PerfilEmpresa() {
+		setTitle("Perfil Empresa");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 779, 526);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBackground(Color.WHITE);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Configuracion", "Editar Cuenta", "Cerrar Sesion"}));
+		comboBox.setBounds(591, 11, 162, 20);
+		contentPane.add(comboBox);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Capacidades", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setBounds(92, 168, 611, 241);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+		rdbtnNewRadioButton.setBounds(33, 52, 212, 23);
+		panel_1.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_1.setBounds(33, 97, 212, 23);
+		panel_1.add(rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_2.setBounds(33, 188, 212, 23);
+		panel_1.add(rdbtnNewRadioButton_2);
+		
+		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_3.setBounds(369, 188, 212, 23);
+		panel_1.add(rdbtnNewRadioButton_3);
+		
+		JRadioButton rdbtnNewRadioButton_4 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_4.setBounds(33, 142, 212, 23);
+		panel_1.add(rdbtnNewRadioButton_4);
+		
+		JRadioButton rdbtnNewRadioButton_5 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_5.setBounds(369, 52, 212, 23);
+		panel_1.add(rdbtnNewRadioButton_5);
+		
+		JRadioButton rdbtnNewRadioButton_6 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_6.setBounds(369, 97, 212, 23);
+		panel_1.add(rdbtnNewRadioButton_6);
+		
+		JRadioButton rdbtnNewRadioButton_7 = new JRadioButton("New radio button");
+		rdbtnNewRadioButton_7.setBounds(369, 142, 212, 23);
+		panel_1.add(rdbtnNewRadioButton_7);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(251, 438, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(464, 438, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Buscar Empresa", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(5, 35, 491, 83);
+		contentPane.add(panel);
+		
+		JLabel label = new JLabel("Nombre");
+		label.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 12));
+		label.setBounds(10, 44, 46, 14);
+		panel.add(label);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBackground(SystemColor.inactiveCaption);
+		textField.setBounds(68, 40, 253, 25);
+		panel.add(textField);
+		
+		JButton button = new JButton("Buscar");
+		button.setBackground(Color.WHITE);
+		button.setBounds(367, 40, 89, 25);
+		panel.add(button);
 	}
-
 }
--- P1 ISC/src/Visual/PerfilUsuarios.java
+++ P1 ISC/src/Visual/PerfilUsuarios.java
@@ -1,31 +1,121 @@
 package Visual;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 
-import Logic.Persona;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 public class PerfilUsuarios extends JFrame {
 
 	private JPanel contentPane;
- PerfilUsuarios(Persona persona) {
-	    initComponents();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
-		
-	}
- private void initComponents() {
-	 contentPane = new JPanel();
-	 
- }
+	private JTextField textField;
 
+	/**
+	 * Launch the application.
+	 */
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public PerfilUsuarios() {
+		setTitle("Perfil Usuario");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 821, 591);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Buscar Empresa", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(10, 25, 491, 83);
+		contentPane.add(panel);
+		
+		JLabel label = new JLabel("Nombre");
+		label.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 12));
+		label.setBounds(10, 44, 46, 14);
+		panel.add(label);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBackground(SystemColor.inactiveCaption);
+		textField.setBounds(68, 40, 253, 25);
+		panel.add(textField);
+		
+		JButton button = new JButton("Buscar");
+		button.setBackground(Color.WHITE);
+		button.setBounds(367, 40, 89, 25);
+		panel.add(button);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Requisitos", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setBounds(92, 191, 611, 241);
+		contentPane.add(panel_1);
+		
+		JRadioButton radioButton = new JRadioButton("New radio button");
+		radioButton.setBounds(33, 52, 212, 23);
+		panel_1.add(radioButton);
+		
+		JRadioButton radioButton_1 = new JRadioButton("New radio button");
+		radioButton_1.setBounds(33, 97, 212, 23);
+		panel_1.add(radioButton_1);
+		
+		JRadioButton radioButton_2 = new JRadioButton("New radio button");
+		radioButton_2.setBounds(33, 188, 212, 23);
+		panel_1.add(radioButton_2);
+		
+		JRadioButton radioButton_3 = new JRadioButton("New radio button");
+		radioButton_3.setBounds(369, 188, 212, 23);
+		panel_1.add(radioButton_3);
+		
+		JRadioButton radioButton_4 = new JRadioButton("New radio button");
+		radioButton_4.setBounds(33, 142, 212, 23);
+		panel_1.add(radioButton_4);
+		
+		JRadioButton radioButton_5 = new JRadioButton("New radio button");
+		radioButton_5.setBounds(369, 52, 212, 23);
+		panel_1.add(radioButton_5);
+		
+		JRadioButton radioButton_6 = new JRadioButton("New radio button");
+		radioButton_6.setBounds(369, 97, 212, 23);
+		panel_1.add(radioButton_6);
+		
+		JRadioButton radioButton_7 = new JRadioButton("New radio button");
+		radioButton_7.setBounds(369, 142, 212, 23);
+		panel_1.add(radioButton_7);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Configuracion", "Editar Cuenta", "Cerrar Sesion"}));
+		comboBox.setBackground(Color.WHITE);
+		comboBox.setBounds(633, 11, 162, 20);
+		contentPane.add(comboBox);
+		
+		JButton button_1 = new JButton("New button");
+		button_1.setBounds(250, 462, 89, 23);
+		contentPane.add(button_1);
+		
+		JButton button_2 = new JButton("New button");
+		button_2.setBounds(459, 462, 89, 23);
+		contentPane.add(button_2);
+	}
 }
