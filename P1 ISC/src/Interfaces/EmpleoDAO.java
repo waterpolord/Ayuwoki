732eec40ee494733c521dc90b35f9a7c9c7f7fc0
Nelson David <Nelson David@N-DAVID-DURAN-MOREL>
2019-11-30 19:04:34 -0400

    Registro en clase Empleo y el patrón EmpleoDAO.

---
+++ P1 ISC/src/Interfaces/EmpleoDAO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+import Logic.*;
+
+/**
+*
+* @author r_rok
+*/
+public interface EmpleoDAO {
+	public void Registrar(Empleo Nuevo) throws Exception;
+    public void Modificar(Empleo Nuevo) throws Exception;
+    public void Eliminar(Empleo Nuevo) throws Exception;
+    public void RetornarEmpleo() throws Exception;
+}
--- P1 ISC/src/Logic/Empleo.java
+++ P1 ISC/src/Logic/Empleo.java
@@ -1,10 +1,16 @@
 package Logic;
 
+import static Conexion.Connect.getConexion;
+
 import java.io.File;
 import java.io.Serializable;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
-public class Empleo implements Serializable{
+import Interfaces.EmpleoDAO;
+
+public class Empleo implements Serializable, EmpleoDAO{
 	
 	private int monto;
 	private Boolean[] habilidades = new Boolean[10];
@@ -33,5 +39,28 @@ public class Empleo implements Serializable{
 		this.cualidad = cualidad;
 	}
 
-	
+	@Override
+    public void Registrar(Empleo Nuevo) throws Exception {
+    	CallableStatement entrada  = getConexion().prepareCall("{Call GuardarSolicitudPersona(?,?,?,?,?,?,?,?,?,?,?,?)}");
+        
+        for(int i = 1, i2 = 0; i < 11; i++, i2++ ) {
+        	entrada.setBoolean(i,Nuevo.habilidades[i2]);
+        }
+        entrada.setString(11, Nuevo.cualidad);
+        entrada.setInt(12, monto);
+        entrada.execute();
+    }
+
+    @Override
+    public void Modificar(Empleo Nuevo) throws Exception {
+    }
+
+    @Override
+    public void Eliminar(Empleo Nuevo) throws Exception {
+    }
+
+    @Override
+    public void RetornarEmpleo() throws Exception {
+   
+    }
 }
